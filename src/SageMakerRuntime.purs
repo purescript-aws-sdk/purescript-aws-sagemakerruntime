937c265e767049da70c6177b552802b5a0b6785f
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 18:20:37 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/SageMakerRuntime.purs
+++ src/SageMakerRuntime.purs
@@ -20,12 +20,12 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "SageMakerRuntime" :: String
-
 
 -- | <p>After you deploy a model into production using Amazon SageMaker hosting services, your client applications use this API to get inferences from the model hosted at the specified endpoint. </p> <p>For an overview of Amazon SageMaker, see <a href="http://docs.aws.amazon.com/sagemaker/latest/dg/how-it-works.html">How It Works</a> </p> <p> Amazon SageMaker strips all POST headers except those supported by the API. Amazon SageMaker might add additional headers. You should not rely on the behavior of headers outside those enumerated in the request syntax. </p>
 invokeEndpoint :: forall eff. InvokeEndpointInput -> Aff (exception :: EXCEPTION | eff) InvokeEndpointOutput
-invokeEndpoint = Request.request serviceName "invokeEndpoint" 
+invokeEndpoint = Request.request service method  where
+    service = Request.ServiceName "SageMakerRuntime"
+    method = Request.MethodName "invokeEndpoint"
 
 
 newtype BodyBlob = BodyBlob String
